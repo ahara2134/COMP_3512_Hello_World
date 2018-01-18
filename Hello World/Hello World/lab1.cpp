c48596f553a87e95750e2fd63714355bc0bd21de
aharaguchi <akharaguchi@gmail.com>
2018-01-18 15:18:23 -0800

    implemented 2 functions for fib and gcd

--- Hello World/Hello World/lab1.cpp
+++ Hello World/Hello World/lab1.cpp
@@ -8,8 +8,21 @@
 // RETURN: the greatest common denominator of a and b.
 int gcd(const int a, const int b)
 {
-	// Replace this line with your code (have fun!).
-	return 0;
+	int gcd{ 1 };
+
+	if (a == b) {
+		gcd = a;
+	}
+
+	int first_num = a;
+	int second_num = b;
+
+	for (int i = 2; i <= first_num && i <= second_num; i++) {
+		if (first_num % i == 0 && second_num % i == 0) {
+			gcd = i;
+		}
+	}
+	return gcd;
 }
 
 // Fibonacci
@@ -18,6 +31,9 @@ int gcd(const int a, const int b)
 // RETURN: the nth positive integer in the Fibonacci sequence.
 long long fibonacci(const int n)
 {
-	// Replace this line with your code (challenge: do this with recursion!).
-	return 0;
+	if (n <= 1) {
+		return n;
+	}
+	int fib = n;
+	return fibonacci(fib - 1) + fibonacci(fib - 2);
 }
